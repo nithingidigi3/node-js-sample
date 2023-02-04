c3d815ea9600b4e102574d3ee9e600d9589ddd86
Nithin Gidigi <121715127+nithingidigi3@users.noreply.github.com>
2023-02-04 12:10:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var router = express.Router();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'BackSpace Academy' });
+});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+module.exports = router;
